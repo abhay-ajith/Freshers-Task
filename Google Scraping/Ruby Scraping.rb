be8692920f3e6e0ff2f4dcf5eb892d9d62dfe35a
abhay-ajith <54947155+abhay-ajith@users.noreply.github.com>
2019-09-24 23:41:28 +0530

    Add files via upload

---
+++ Google Scraping/Ruby Scraping.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby -w
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+document= Nokogiri::HTML(open("https://www.google.com/search?q=linux"))
+nd=document.xpath("//a")
+nod.each do |nd|
+puts nd.text
+end
